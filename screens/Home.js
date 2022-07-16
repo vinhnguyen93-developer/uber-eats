84a6ae9913c8abb78766c602c2589f7b98c556c8
Vinhnguyen93-developer <developerthienkim@gmail.com>
2022-07-16 21:52:29 +0700

    Header Categories Search bar and Home screen

---
+++ screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+import HeaderTabs from '../components/HeaderTabs';
+import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
+
+export default function Home() {
+  return (
+    <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
+      <View style={{ backgroundColor: 'white', padding: 15 }}>
+        <HeaderTabs />
+        <SearchBar />
+      </View>
+      <Categories />
+    </SafeAreaView>
+  );
+}
